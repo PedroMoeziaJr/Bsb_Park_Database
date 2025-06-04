09cbf9bc8e002504d248c9ba3f0cb0e9cabb7a9c
Pedro Ivo Moézia de Lima Junior <112977342+PedroMoeziaJr@users.noreply.github.com>
2025-06-04 16:17:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import date
 
 # Dados do Supabase
 SUPABASE_URL = "https://clyuxrlqbkdadhkpzaly.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNseHV4cmxxYmtkYWRoa3B6YWx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5Nzg3NjgsImV4cCI6MjA2NDU1NDc2OH0.aMgo3gBA9Rb_H-Oex2nQ8SccmSfMNKv8TwyAixan2Wk"
 
 # Conexão com o Supabase
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
