68da3fe1e694b73aeacf728cc60caa58d44d4bfe
Pedro Ivo Moézia de Lima Junior <112977342+PedroMoeziaJr@users.noreply.github.com>
2025-06-09 16:04:26 -0300

    Update folha.py

--- folha.py
+++ folha.py
@@ -3,8 +3,9 @@ from supabase import create_client, Client
 import os
 
 # Configuração do Supabase - substitua pelas suas variáveis
-url = os.environ.get("SUPABASE_URL")
-key = os.environ.get("SUPABASE_KEY")
+url = url = "https://clxuxrlqbkdadhkpzaly.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNseHV4cmxxYmtkYWRoa3B6YWx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5Nzg3NjgsImV4cCI6MjA2NDU1NDc2OH0.aMgo3gBA9Rb_H-Oex2nQ8SccmSfMNKv8TwyAixan2Wk"
+
 supabase: Client = create_client(url, key)
 
 st.title("Folha de Pagamento")
